41b1cf4cecf2d9d7cd962c57003c70ee10257857
Adam Crockett <arc1224@gmail.com>
2022-11-20 18:46:55 -0800

    server side getPost implementation

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   getPostsBySearch,
   getPosts,
+  getPost,
   createPost,
   updatePost,
   deletePost,
@@ -11,6 +12,7 @@ import auth from '../middleware/auth.js';
 const router = express.Router();
 
 router.get('/', getPosts);
+router.get('/:id', getPost);
 router.get('/search', getPostsBySearch);
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
