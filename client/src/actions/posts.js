91d5994b2128cd067d3a03794f98426fc95a6dba
Adam Crockett <arc1224@gmail.com>
2022-11-20 17:58:25 -0800

    Loading actions added

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -4,14 +4,19 @@ import {
   CREATE,
   UPDATE,
   DELETE,
+  START_LOADING,
+  END_LOADING,
 } from '../constants/actionTypes';
 import * as api from '../api';
 
 // Action Creators
-export const getPosts = () => async (dispatch) => {
+export const getPosts = (page) => async (dispatch) => {
   try {
-    const { data } = await api.fetchPosts();
+    dispatch({ type: START_LOADING });
+    const { data } = await api.fetchPosts(page);
+
     dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error);
   }
@@ -19,10 +24,12 @@ export const getPosts = () => async (dispatch) => {
 
 export const getPostBySearch = (searchQuery) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
     const {
       data: { data },
     } = await api.fetchPostsBySearch(searchQuery);
     dispatch({ type: FETCH_BY_SEARCH, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error);
   }
@@ -30,8 +37,10 @@ export const getPostBySearch = (searchQuery) => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
     const { data } = await api.creatPost(post);
     dispatch({ type: CREATE, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error);
   }
