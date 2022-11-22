b462f499a77f8c3e6d87ff1701eb27982c3b50d7
Adam Crockett <arc1224@gmail.com>
2022-11-22 09:18:12 -0800

    elevation change on sign in notification

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -49,7 +49,7 @@ function Form({ currentId, setCurrentId }) {
 
   if (!user?.result?.name) {
     return (
-      <Paper className={classes.paper}>
+      <Paper className={classes.paper} elevation={6}>
         <Typography variant='h6' align='center'>
           Please sign in to create your own memories and like other people's
           memories.
