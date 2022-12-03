e6ddbe1e51a43c93f1c29322bd3fd3b8db07cfd8
Adam Crockett <arc1224@gmail.com>
2022-12-02 17:44:51 -0800

    remove catch all change on server

--- server/index.js
+++ server/index.js
@@ -14,13 +14,13 @@ const CONNECTION_URL = process.env.CONNECTION_URL;
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+// });
 
-app.use(cors());
+// app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
